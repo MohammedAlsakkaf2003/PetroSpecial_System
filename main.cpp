482679c9664be13abbf2f8e29505c2dd191972f5
Mohammed Alsakkaf <zoer4019@gmail.com>
2025-11-26 03:55:20 +0800

    Initial commit: Basic system structure

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "=== Welcome to Petro Special System ===" << endl;
+    cout << "Owner: Mohammed Alsakkaf" << endl;
+    cout << "System Status: Active" << endl;
+    return 0;
+}
