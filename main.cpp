a5e64f59cee065b633e6222bfce5606471c31a8d
Mohammed Alsakkaf <zoer4019@gmail.com>
2025-11-26 04:18:36 +0800

    I improved code to recive input from user

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "=== Welcome to Petro Special System ===" << endl;
-    cout << "Owner: Mohammed Alsakkaf" << endl;
-    cout << "System Status: Active" << endl;
+    int oil_stock = 100;
+    int tire_stock = 50;
+    int battery_stock = 20;
+    
+    int choice;
+
+    cout << "=== Petro Special Inventory System v1.0 ===" << endl;
+    cout << "Welcome, Eng. Mohammed." << endl;
+    cout << "---------------------------------------" << endl;
+    cout << "Current Stock:" << endl;
+    cout << "1. Oils: " << oil_stock << " cans" << endl;
+    cout << "2. Tires: " << tire_stock << " units" << endl;
+    cout << "3. Batteries: " << battery_stock << " units" << endl;
+    cout << "---------------------------------------" << endl;
+    
+    cout << "Please select a product to manage (1-3): ";
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "You selected: Oils." << endl;
+    } else if (choice == 2) {
+        cout << "You selected: Tires." << endl;
+    } else if (choice == 3) {
+        cout << "You selected: Batteries." << endl;
+    } else {
+        cout << "Invalid choice!" << endl;
+    }
+
     return 0;
 }
